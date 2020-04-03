09789d8f3225dd3bad2d648c4bc5190ce9d4e52d
Minilay8 <minilie88@gmail.com>
2020-04-04 04:25:02 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
     var i = 0;
 var text = ["Congratulations!", "You are 18 now!", "We have prepared for you a quest from Chiki company", "Just click anywhere to start"];
     $("html").click(function(){
+        if(i == text.length)
+        {
+            location.replace("./hub.html")
+        }
         setTimeout(function(){$("#main").fadeOut(500)}, 0);
         setTimeout(function(){$('#main').text(text[i]); i++}, 500);
         setTimeout(function(){$("#main").fadeIn(500)}, 600);
